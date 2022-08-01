ae6e11c4074d20e56dc913acdf3b59cffbd98dd3
hayen1112 <18130295@st.hcmuaf.edu.vn>
2022-08-01 16:50:06 +0700

    giao dien myoder

---
+++ app-news-Pro-1/src/app/components/shopping_cart/my_oder/my-oder/my-oder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-oder',
+  templateUrl: './my-oder.component.html',
+  styleUrls: ['./my-oder.component.scss']
+})
+export class MyOderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
