2f5882b8ce065da5d6ccc0fc3f616914de84497c
hayen1112 <18130295@st.hcmuaf.edu.vn>
2022-08-01 23:19:11 +0700

    giao dien tim kiem

--- ChuyenDeWeb-Pro-1/src/main/java/com/example/mypkg/repository/ProductRepository.java
+++ ChuyenDeWeb-Pro-1/src/main/java/com/example/mypkg/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.example.mypkg.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.mypkg.entity.Product;
@@ -14,4 +16,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	public Optional<Product> findByIdAndIsDelete(Long productId, boolean isDelete);
 
 	public List<Product> findByProductTypeAndIsDelete(ProductType productType, boolean isDelete);
+	
+	
 }
