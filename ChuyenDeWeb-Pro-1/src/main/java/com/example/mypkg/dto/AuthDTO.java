f03ce0e11994d430f9c2ac485f4affd93b5b66d1
truongvanxinh <18130292@st.hcmuaf.edu.vn>
2022-08-01 16:42:06 +0700

    .

--- ChuyenDeWeb-Pro-1/src/main/java/com/example/mypkg/dto/AuthDTO.java
+++ ChuyenDeWeb-Pro-1/src/main/java/com/example/mypkg/dto/AuthDTO.java
@@ -10,8 +10,25 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class AuthDTO {
 	private String token;
 	private List<String> roles;
+	public AuthDTO(String token, List<String> roles) {
+		super();
+		this.token = token;
+		this.roles = roles;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public List<String> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+	
 }
